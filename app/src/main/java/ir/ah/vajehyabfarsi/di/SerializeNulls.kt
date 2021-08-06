3142acefceac04e11fd77cc51260a5678816d0e6
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 11:24:02 +0430

    add SerializeNulls.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/di/SerializeNulls.kt
@@ -0,0 +1,29 @@
+package ir.ah.vajehyabfarsi.di
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonQualifier
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import java.lang.reflect.Type
+
+@Retention(AnnotationRetention.RUNTIME)
+@JsonQualifier
+annotation class SerializeNulls {
+    companion object {
+        var JSON_ADAPTER_FACTORY: JsonAdapter.Factory = object : JsonAdapter.Factory {
+            override fun create(
+                type: Type,
+                annotations: Set<Annotation?>,
+                moshi: Moshi
+            ): JsonAdapter<*>? {
+                val nextAnnotations =
+                    Types.nextAnnotations(
+                        annotations,
+                        SerializeNulls::class.java
+                    )
+                        ?: return null
+                return moshi.nextAdapter<Any>(this, type, nextAnnotations).serializeNulls()
+            }
+        }
+    }
+}
