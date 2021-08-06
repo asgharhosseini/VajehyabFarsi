4d1489563e3617456a32fb77bb76a8924020aede
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 16:50:26 +0430

    add HomeFragment.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package ir.ah.vajehyabfarsi.ui.fragment.home
+
+import ir.ah.vajehyabfarsi.*
+import ir.ah.vajehyabfarsi.base.*
+
+class HomeFragment:
+    BaseFragment<HomeViewModel>(R.layout.fragment_home, HomeViewModel::class)
+{
+    override fun observeData() {
+        super.observeData()
+    }
+
+    override fun setUpViews() {
+        super.setUpViews()
+    }
+}
