4c9d26f97a4627505e9228b644773540c8e9c983
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 16:51:36 +0430

    add SearchFragment.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/search/SearchFragment.kt
@@ -0,0 +1,16 @@
+package ir.ah.vajehyabfarsi.ui.fragment.search
+
+import ir.ah.vajehyabfarsi.*
+import ir.ah.vajehyabfarsi.base.*
+
+class SearchFragment:
+    BaseFragment<SearchViewModel>(R.layout.fragment_search, SearchViewModel::class)
+{
+    override fun observeData() {
+        super.observeData()
+    }
+
+    override fun setUpViews() {
+        super.setUpViews()
+    }
+}
