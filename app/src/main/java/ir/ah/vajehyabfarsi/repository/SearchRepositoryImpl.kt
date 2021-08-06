104d15bdc18a71a08163842d2e4a2f8c20af2ca3
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 12:20:16 +0430

    add SearchRepositoryImpl.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/repository/SearchRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package ir.ah.vajehyabfarsi.repository
+
+import ir.ah.vajehyabfarsi.data.model.response.*
+import ir.ah.vajehyabfarsi.data.remot.*
+import ir.ah.vajehyabfarsi.other.util.*
+import ir.ah.vajehyabfarsi.other.wrapper.*
+
+class SearchRepositoryImpl(private val api: ApiService):SearchRepository {
+    override suspend fun getWord(query: String, filter: String): Resource<VajehResponse> =
+        safeApiCall { api.getWord(query,filter) }
+
+
+    override suspend fun getSearchVajeh(query: String, filter: String): Resource<VajehResponse> =
+        safeApiCall { api.getSearchVajeh(query,filter) }
+}
