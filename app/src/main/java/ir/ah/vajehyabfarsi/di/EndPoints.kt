de3fbef30c6eebf290e205a44c67ece12e9509ef
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 12:19:39 +0430

    add EndPoints.kt

--- app/src/main/java/ir/ah/vajehyabfarsi/di/EndPoints.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/di/EndPoints.kt
@@ -1,6 +1,7 @@
 package ir.ah.vajehyabfarsi.di
 
 object EndPoints {
-    const val BASE_URL = "BuildConfig.STAGE_BASE_URL"
+    const val BASE_URL = "http://api.vajehyab.com/v3/"
+    const val TOKEN = "65317.U2I1lqwCsekd4bCm1fWfzkkd7goKxqoDUKYdHoyP"
 
 }
