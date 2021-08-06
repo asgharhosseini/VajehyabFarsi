ab2afd7f383ba518ef3978bc668fc0ba746cce00
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 12:20:06 +0430

    add SearchRepository.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/repository/SearchRepository.kt
@@ -0,0 +1,12 @@
+package ir.ah.vajehyabfarsi.repository
+
+import ir.ah.vajehyabfarsi.data.model.response.*
+import ir.ah.vajehyabfarsi.di.*
+import ir.ah.vajehyabfarsi.other.wrapper.*
+import retrofit2.Response
+import retrofit2.http.*
+
+interface SearchRepository {
+    suspend fun  getWord(query:String,filter:String):Resource<VajehResponse>
+    suspend  fun  getSearchVajeh(query:String,filter:String):Resource<VajehResponse>
+}
