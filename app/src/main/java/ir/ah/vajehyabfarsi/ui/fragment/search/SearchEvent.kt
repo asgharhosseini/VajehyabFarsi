7b657b5b1fac4c1ea23a3aa635d650da2c8246b3
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-06 16:51:01 +0430

    add SearchEvent.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/search/SearchEvent.kt
@@ -0,0 +1,9 @@
+package ir.ah.vajehyabfarsi.ui.fragment.search
+
+import ir.ah.vajehyabfarsi.other.wrapper.*
+
+sealed class SearchEvent {
+
+        object searchQueryIsEmpty : SearchEvent()
+        data class ShowError(val failure: FailureInterface?) : SearchEvent()
+    }
