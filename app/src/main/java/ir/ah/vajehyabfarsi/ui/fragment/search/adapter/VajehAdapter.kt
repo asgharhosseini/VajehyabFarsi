16f8a7ba49c8a1af07098d9ce03ff550bddf720e
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-07 20:05:41 +0430

    add base VajehAdapter.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/search/adapter/VajehAdapter.kt
@@ -0,0 +1,46 @@
+package ir.ah.vajehyabfarsi.ui.fragment.search.adapter
+
+import android.view.*
+import androidx.recyclerview.widget.*
+import androidx.viewbinding.*
+import ir.ah.vajehyabfarsi.base.*
+import ir.ah.vajehyabfarsi.data.model.response.*
+import ir.ah.vajehyabfarsi.databinding.*
+
+class VajehAdapter:BaseAdapter<Vajeh, VajehAdapter.VajehViewHolder>() {
+
+    private var vajehEventListener:VajehEventListener?=null
+   inner class VajehViewHolder(private val binding: ItemVajehBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(vajeh: Vajeh) {
+            binding.apply {
+                textView.text=vajeh.text
+                imageView2.setOnClickListener {
+                    vajehEventListener?.onFavorite(vajeh)
+                }
+                imageView2.setOnClickListener {
+                    vajehEventListener?.onRemoveFavorite(vajeh)
+                }
+
+            }
+        }
+
+    }
+    fun setOnOrderItemEventListener(vajehEventListener:VajehEventListener) {
+        this.vajehEventListener = vajehEventListener
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VajehViewHolder {
+        val binding = ItemVajehBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VajehViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VajehViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    interface VajehEventListener {
+        fun onFavorite(vajeh: Vajeh)
+        fun onRemoveFavorite(vajeh: Vajeh)
+    }
+}
