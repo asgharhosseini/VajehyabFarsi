aa38ade6a305eda6bee03eb169f78d3c6c72c0f2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-08 20:50:58 +0430

    add getAllHistory function in HomeViewModel.kt

--- app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/home/HomeViewModel.kt
@@ -1,22 +1,24 @@
 package ir.ah.vajehyabfarsi.ui.fragment.home
 
-import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.*
 import ir.ah.vajehyabfarsi.base.*
-import ir.ah.vajehyabfarsi.data.model.response.*
-import ir.ah.vajehyabfarsi.other.wrapper.*
-import ir.ah.vajehyabfarsi.repository.*
-import ir.ah.vajehyabfarsi.ui.fragment.search.*
+import ir.ah.vajehyabfarsi.data.model.*
+import ir.ah.vajehyabfarsi.repository.history.*
+import ir.ah.vajehyabfarsi.repository.search.*
+import ir.ah.vajehyabfarsi.ui.fragment.home.adapter.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.*
 import javax.inject.*
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     val mainCoroutineDispatcher: CoroutineDispatcher,
-    private val searchRepository: SearchRepository,
+    private val historyRepository: HistoryRepository,
 ) : BaseViewModel(mainCoroutineDispatcher) {
+
+
+
+    fun getAllHistory() =historyRepository.getAllHistory()
 
 }
 
