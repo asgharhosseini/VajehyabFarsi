4a4a8b094b4f883f18c0b0bdf6202bc5b1ebbc07
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-08 15:32:39 +0430

    add data base to repository

--- app/src/main/java/ir/ah/vajehyabfarsi/repository/SearchRepository.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/repository/SearchRepository.kt
@@ -1,12 +1,20 @@
 package ir.ah.vajehyabfarsi.repository
 
+import androidx.lifecycle.*
+import ir.ah.vajehyabfarsi.data.model.*
 import ir.ah.vajehyabfarsi.data.model.response.*
 import ir.ah.vajehyabfarsi.di.*
 import ir.ah.vajehyabfarsi.other.wrapper.*
+import kotlinx.coroutines.flow.*
 import retrofit2.Response
 import retrofit2.http.*
 
 interface SearchRepository {
     suspend fun  getWord(query:String,filter:String):Resource<VajehResponse>
     suspend  fun  getSearchVajeh(query:String,filter:String):Resource<VajehResponse>
+
+    suspend fun insertVajeh(vajeh: Vajeh)
+    suspend fun deleteItem(id: String)
+    fun getAllFavorite(): Flow<List<Vajeh>>
+    fun checkVajehsFavorite(id: String): LiveData<Boolean>
 }
--- app/src/main/java/ir/ah/vajehyabfarsi/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/repository/SearchRepositoryImpl.kt
@@ -1,15 +1,32 @@
 package ir.ah.vajehyabfarsi.repository
 
+import androidx.lifecycle.*
+import androidx.room.*
+import ir.ah.vajehyabfarsi.data.local.*
+import ir.ah.vajehyabfarsi.data.model.*
 import ir.ah.vajehyabfarsi.data.model.response.*
 import ir.ah.vajehyabfarsi.data.remot.*
 import ir.ah.vajehyabfarsi.other.util.*
 import ir.ah.vajehyabfarsi.other.wrapper.*
+import kotlinx.coroutines.flow.*
 
-class SearchRepositoryImpl(private val api: ApiService):SearchRepository {
+class SearchRepositoryImpl(
+    private val api: ApiService,
+    private val database:VajehDao
+) : SearchRepository {
     override suspend fun getWord(query: String, filter: String): Resource<VajehResponse> =
-        safeApiCall { api.getWord(query,filter) }
+        safeApiCall { api.getWord(query, filter) }
 
 
-    override suspend fun getSearchVajeh(query: String,filter:String): Resource<VajehResponse> =
+    override suspend fun getSearchVajeh(query: String, filter: String): Resource<VajehResponse> =
         safeApiCall { api.getSearchVajeh(query) }
+
+
+    override suspend fun insertVajeh(vajeh: Vajeh)=database.insertVajeh(vajeh)
+    override suspend fun deleteItem(id: String)=database.deleteItem(id)
+    override fun getAllFavorite(): Flow<List<Vajeh>> =database.getAllFavorite()
+    override fun checkVajehsFavorite(id: String): LiveData<Boolean> =database.checkVajehsFavorite(id)
+
+
+
 }
