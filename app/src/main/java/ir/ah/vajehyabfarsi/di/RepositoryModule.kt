192bf2bcb7012f44d38f222f8cb8393ac81eaa95
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-08 16:39:22 +0430

    add HistoryRepository provider

--- app/src/main/java/ir/ah/vajehyabfarsi/di/RepositoryModule.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/di/RepositoryModule.kt
@@ -3,7 +3,9 @@ package ir.ah.vajehyabfarsi.di
 import dagger.*
 import dagger.hilt.*
 import dagger.hilt.components.*
+import ir.ah.vajehyabfarsi.data.local.*
 import ir.ah.vajehyabfarsi.data.remot.*
+import ir.ah.vajehyabfarsi.repository.history.*
 import ir.ah.vajehyabfarsi.repository.search.*
 import javax.inject.*
 
@@ -13,8 +15,12 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    internal fun provideSearchRepository(api:ApiService ): SearchRepository =
-        SearchRepositoryImpl(api)
+    internal fun provideSearchRepository(api:ApiService,database: VajehDao): SearchRepository =
+        SearchRepositoryImpl(api,database)
+    @Provides
+    @Singleton
+    internal fun provideHistoryRepository(database: HistoryDao): HistoryRepository =
+        HistoryRepositoryImpl(database)
 
 
 }
