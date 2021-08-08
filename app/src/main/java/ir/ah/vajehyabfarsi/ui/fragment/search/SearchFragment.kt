7f33f7afe4c8e70e4728cce221b229c1782e4b65
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-08 11:27:49 +0430

    add adapter in SearchFragment.kt

--- app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/search/SearchFragment.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/search/SearchFragment.kt
@@ -3,22 +3,29 @@ package ir.ah.vajehyabfarsi.ui.fragment.search
 import android.util.*
 import android.widget.*
 import androidx.lifecycle.*
+import androidx.recyclerview.widget.*
 import dagger.hilt.android.*
 import ir.ah.vajehyabfarsi.R
 import ir.ah.vajehyabfarsi.base.*
+import ir.ah.vajehyabfarsi.data.model.response.*
 import ir.ah.vajehyabfarsi.databinding.*
 import ir.ah.vajehyabfarsi.other.*
 import ir.ah.vajehyabfarsi.other.util.*
 import ir.ah.vajehyabfarsi.other.util.Constance.TAG
 import ir.ah.vajehyabfarsi.other.util.Constance.filterName
 import ir.ah.vajehyabfarsi.other.wrapper.*
+import ir.ah.vajehyabfarsi.ui.fragment.search.adapter.*
 import kotlinx.coroutines.flow.*
 
 
 @AndroidEntryPoint
 class SearchFragment :
-    BaseFragment<SearchViewModel>(R.layout.fragment_search, SearchViewModel::class) {
+    BaseFragment<SearchViewModel>(R.layout.fragment_search, SearchViewModel::class),
+VajehAdapter.VajehEventListener{
     private val binding by viewBinding(FragmentSearchBinding::bind)
+
+    private val vajehAdapter by lazy { VajehAdapter() }
+
 
 
     override fun observeData() {
@@ -34,7 +41,7 @@ class SearchFragment :
             vm.searchQuery.value = it
             binding.btnSearch.isEnabled = it.count { it.isDigit() } < 1
         }
-
+        setUpVajehdapter()
 
     }
 
@@ -42,6 +49,13 @@ class SearchFragment :
         binding.btnSearch.setOnClickListener {
           vm.validateSearchQuery()
 
+        }
+    }
+    private fun setUpVajehdapter() {
+        vajehAdapter.setOnItemClickListener { }
+        binding.recyclerView.apply {
+            adapter = vajehAdapter
+            layoutManager=LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
         }
     }
 
@@ -80,14 +94,22 @@ class SearchFragment :
                     is Resource.Loading -> {
                     }
                     is Resource.Success -> {
-
-
+                    vajehAdapter.submitList(event.success.data.results)
+                        Log.e(TAG,event.success.data.results[0].title)
                     }
                     is Resource.Failure -> {
                     }
                 }
             }
         }
+    }
+
+    override fun onFavorite(vajeh: Vajeh) {
+
+    }
+
+    override fun onRemoveFavorite(vajeh: Vajeh) {
+
     }
 
 
