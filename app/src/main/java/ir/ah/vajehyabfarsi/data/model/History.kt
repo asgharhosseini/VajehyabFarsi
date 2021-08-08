a423d45d17685563aa127c5e272e08f220a70efd
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-08 16:37:32 +0430

    add item to History.kt

--- app/src/main/java/ir/ah/vajehyabfarsi/data/model/History.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/data/model/History.kt
@@ -7,21 +7,11 @@ import com.squareup.moshi.*
 @JsonClass(generateAdapter = true)
 @Entity
 data class History(
-    @Json(name = "db")
-    val db: String,
-    @PrimaryKey( )
+    @PrimaryKey(autoGenerate = true )
     @Json(name = "id")
-    val id: String,
-    @Json(name = "num")
-    val num: Int,
-    @Json(name = "pron")
-    val pron: String,
-    @Json(name = "source")
-    val source: String,
-    @Json(name = "text")
-    val text: String,
+    val id: Int=0,
     @Json(name = "title")
     val title: String,
-    @Json(name = "title_en")
-    val titleEn: String
+    @Json(name = "filter")
+    val filter: String,
 )
