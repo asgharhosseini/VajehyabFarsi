b00d94ad1c3ef0845b860b603a07b3a708d7e119
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-09 12:03:23 +0430

    add insertVajeh  and deleteItem in FavoriteViewModel.kt

--- app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/favorite/FavoriteViewModel.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/favorite/FavoriteViewModel.kt
@@ -3,6 +3,7 @@ package ir.ah.vajehyabfarsi.ui.fragment.favorite
 import dagger.hilt.android.lifecycle.*
 import ir.ah.vajehyabfarsi.base.*
 import ir.ah.vajehyabfarsi.data.model.*
+import ir.ah.vajehyabfarsi.data.model.response.*
 import ir.ah.vajehyabfarsi.repository.history.*
 import ir.ah.vajehyabfarsi.repository.search.*
 import ir.ah.vajehyabfarsi.ui.fragment.home.adapter.*
@@ -16,7 +17,8 @@ class FavoriteViewModel @Inject constructor(
     private val repository: SearchRepository,
 ) : BaseViewModel(mainCoroutineDispatcher) {
 
-
+    fun insertVajeh(vajeh: Vajeh) = doInMain { repository.insertVajeh(vajeh) }
+    fun deleteItem(id: String) = doInMain { repository.deleteItem(id) }
     fun getAllFavorite() =repository.getAllFavorite()
 
 
