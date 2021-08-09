9be6c18e0eed2295d1295c0b05739357efbd1aa2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-09 11:09:31 +0430

    add FavoriteFragment.kt

---
+++ app/src/main/java/ir/ah/vajehyabfarsi/ui/fragment/favorite/FavoriteFragment.kt
@@ -0,0 +1,8 @@
+package ir.ah.vajehyabfarsi.ui.fragment.favorite
+
+import ir.ah.vajehyabfarsi.*
+import ir.ah.vajehyabfarsi.base.*
+
+class FavoriteFragment : BaseFragment<FavoriteViewModel>(R.layout.fragment_favorite, FavoriteViewModel::class
+) {
+}
