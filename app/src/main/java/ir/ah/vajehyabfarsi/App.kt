2266fd6ee2e497c4a7951c77b7b5f4fe5a0aad6c
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-11 12:01:33 +0430

    add NotificationManager in App.kt

--- app/src/main/java/ir/ah/vajehyabfarsi/App.kt
+++ app/src/main/java/ir/ah/vajehyabfarsi/App.kt
@@ -1,8 +1,29 @@
 package ir.ah.vajehyabfarsi
 
 import android.app.*
+import android.os.*
 import dagger.hilt.android.*
+import ir.ah.vajehyabfarsi.other.util.Constance.NOTIFICATION_CHANNEL_ID
+import ir.ah.vajehyabfarsi.other.util.Constance.NOTIFICATION_CHANNEL_NAME
 
 @HiltAndroidApp
 class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            var notificationManager: NotificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            var notificationChannel: NotificationChannel =
+                NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    NOTIFICATION_CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(notificationChannel)
+            }
+
+        }
+    }
 }
